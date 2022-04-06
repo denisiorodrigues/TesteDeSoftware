185e30a4ea20cf1ec4efe4f7d8da4e54325292d7
= <=>
2022-04-06 00:06:59 -0300

    Add project files.

---
+++ Demo.Test/CalculadoraTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.Test
+{
+
+    public class CalculadoraTest
+    {
+        [Fact]
+        public void Calculadora_Somar_ReotnrarValorSoma()
+        {
+            //Arrange
+            var calculadora = new Claculadora();
+
+            //Acet
+            var resultado = calculadora.Somar(2, 3);
+
+            //Assert
+            Assert.Equal(5, resultado);
+        }
+    }
+}
---
+++ Demo/Claculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Claculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
